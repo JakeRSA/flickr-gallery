000c58db69f66cd4c04981163716c07caf2da1ef
JakeRSA <jakenudels@gmail.com>
2021-01-17 12:36:40 +0200

    implemented functionality for delete image button

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import FontAwesome from 'react-fontawesome';
-import './Image.scss';
+import React from "react";
+import PropTypes from "prop-types";
+import FontAwesome from "react-fontawesome";
+import "./Image.scss";
 
 class Image extends React.Component {
   static propTypes = {
     dto: PropTypes.object,
-    galleryWidth: PropTypes.number
+    galleryWidth: PropTypes.number,
   };
 
   constructor(props) {
@@ -14,24 +14,29 @@ class Image extends React.Component {
     this.calcImageSize = this.calcImageSize.bind(this);
     this.state = {
       size: 200,
-      rotation: 0
+      rotation: 0,
+      hidden: false,
     };
   }
 
   calcImageSize() {
-    const {galleryWidth} = this.props;
+    const { galleryWidth } = this.props;
     const targetSize = 200;
     const imagesPerRow = Math.round(galleryWidth / targetSize);
-    const size = (galleryWidth / imagesPerRow);
+    const size = galleryWidth / imagesPerRow;
     this.setState({
-      size
+      size,
     });
+  }
+
+  hideImage() {
+    this.setState({ hidden: true });
   }
 
   rotateImage() {
     if (this.state.rotation < 270) {
-      this.setState(state => ({rotation: state.rotation + 90}))
-    } else this.setState({rotation: 0})
+      this.setState((state) => ({ rotation: state.rotation + 90 }));
+    } else this.setState({ rotation: 0 });
   }
 
   componentDidMount() {
@@ -48,17 +53,34 @@ class Image extends React.Component {
         className="image-root"
         style={{
           backgroundImage: `url(${this.urlFromDto(this.props.dto)})`,
-          width: this.state.size + 'px',
-          height: this.state.size + 'px',
-          transform: `rotate(${this.state.rotation}deg)`
+          width: this.state.size + "px",
+          height: this.state.size + "px",
+          transform: `rotate(${this.state.rotation}deg)`,
+          display: this.state.hidden ? "none" : "inline-block",
         }}
+      >
+        <div
+          style={{
+            transform: `rotate(-${this.state.rotation}deg)`,
+          }}
         >
-        <div style={{
-          transform: `rotate(-${this.state.rotation}deg)`
-        }}>
-          <FontAwesome className="image-icon" name="sync-alt" title="rotate" onClick={()=> {this.rotateImage()}}/>
-          <FontAwesome className="image-icon" name="trash-alt" title="delete"/>
-          <FontAwesome className="image-icon" name="expand" title="expand"/>
+          <FontAwesome
+            className="image-icon"
+            name="sync-alt"
+            title="rotate"
+            onClick={() => {
+              this.rotateImage();
+            }}
+          />
+          <FontAwesome
+            className="image-icon"
+            name="trash-alt"
+            title="delete"
+            onClick={() => {
+              this.hideImage();
+            }}
+          />
+          <FontAwesome className="image-icon" name="expand" title="expand" />
         </div>
       </div>
     );
