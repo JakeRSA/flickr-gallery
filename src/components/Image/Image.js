fbaa28863efdab644a9204c68e8da18dfa974c93
JakeRSA <jakenudels@gmail.com>
2021-01-19 20:57:09 +0200

    added image filters

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -17,6 +17,10 @@ class Image extends React.Component {
       rotation: 0,
       hidden: false,
       showLarge: false,
+      brightness: 100,
+      contrast: 100,
+      hue: 0,
+      saturation: 100,
     };
   }
 
@@ -48,6 +52,7 @@ class Image extends React.Component {
           height: this.props.imageSize + "px",
           transform: `rotate(${this.state.rotation}deg)`,
           display: this.state.hidden ? "none" : "inline-block",
+          filter: `brightness(${this.state.brightness}%) contrast(${this.state.contrast}%) hue-rotate(${this.state.hue}deg) saturate(${this.state.saturation}%)`,
         }}
       >
         <Modal
@@ -61,7 +66,9 @@ class Image extends React.Component {
             <span className="modal-header">
               <span className="modal-title-span">
                 <p>
-                  {this.props.dto.title ? this.props.dto.title : "<no title>"}
+                  {this.props.dto.title
+                    ? this.props.dto.title
+                    : "untitled image"}
                 </p>
 
                 <a
@@ -86,19 +93,68 @@ class Image extends React.Component {
                 }}
               />
             </span>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href={this.urlFromDto(this.props.dto)}
-            >
-              <img
-                src={this.urlFromDto(this.props.dto)}
-                style={{
-                  maxWidth: "100%",
-                  maxHeight: "100%",
-                }}
-              ></img>
-            </a>
+            <img
+              src={this.urlFromDto(this.props.dto)}
+              style={{
+                maxWidth: "100%",
+                maxHeight: "100%",
+                filter: `brightness(${this.state.brightness}%) contrast(${this.state.contrast}%) hue-rotate(${this.state.hue}deg) saturate(${this.state.saturation}%)`,
+              }}
+            ></img>
+            <span className="modal-footer">
+              <span>
+                <label htmlFor="brightness">brightness</label>
+                <input
+                  id="brightness"
+                  type="range"
+                  min="0"
+                  max="200"
+                  value={this.state.brightness}
+                  onChange={(e) => {
+                    this.setState({ brightness: e.target.value });
+                  }}
+                />
+              </span>
+              <span>
+                <label htmlFor="contrast">contrast</label>
+                <input
+                  id="contrast"
+                  type="range"
+                  min="0"
+                  max="200"
+                  value={this.state.contrast}
+                  onChange={(e) => {
+                    this.setState({ contrast: e.target.value });
+                  }}
+                />
+              </span>
+              <span>
+                <label htmlFor="hue">hue</label>
+                <input
+                  id="hue"
+                  type="range"
+                  min="-180"
+                  max="180"
+                  value={this.state.hue}
+                  onChange={(e) => {
+                    this.setState({ hue: e.target.value });
+                  }}
+                />
+              </span>
+              <span>
+                <label htmlFor="saturation">saturation</label>
+                <input
+                  id="saturation"
+                  type="range"
+                  min="0"
+                  max="200"
+                  value={this.state.saturation}
+                  onChange={(e) => {
+                    this.setState({ saturation: e.target.value });
+                  }}
+                />
+              </span>
+            </span>
           </div>
         </Modal>
         <div
