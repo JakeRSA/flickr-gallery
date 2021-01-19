14933a811c58159150341be8585e44bc92d3e387
JakeRSA <jakenudels@gmail.com>
2021-01-19 15:09:06 +0200

    Revert "implemented draggable image on mousedown"
    This reverts commit ef330f9d191db603cc66c315a2a44bf668533571.

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -15,14 +15,7 @@ class Image extends React.Component {
       rotation: 0,
       hidden: false,
       showLarge: false,
-      dragging: false,
     };
-  }
-
-  handleMouseDown() {
-    this.setState({dragging: true})
-    this.props.onDragImage(this.urlFromDto(this.props.dto))
-    window.console.log('mousedown')
   }
 
   hideImage() {
@@ -54,7 +47,6 @@ class Image extends React.Component {
           transform: `rotate(${this.state.rotation}deg)`,
           display: this.state.hidden ? "none" : "inline-block",
         }}
-        onMouseDown={() => {this.handleMouseDown()}}
       >
         <Modal
           isOpen={this.state.showLarge}
